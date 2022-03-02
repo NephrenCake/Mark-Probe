6f18e9617f19b7a5f84df77d6166a7c1f25cb20e
Moisthis <3157069578@qq.com>
2022-03-02 21:36:50 +0800

    网络

--- detection/Generate_Dataset.py
+++ detection/Generate_Dataset.py
@@ -8,12 +8,12 @@ import cv2 as cv
 import torchvision.transforms.functional as F
 import numpy as np
 
-img_save_source = 'D:\Program data\pythonProject\deeplabv3-plus-pytorch-main\VOCdevkit\VOC2007\JPEGImages'
-seg_save_source = 'D:\Program data\pythonProject\deeplabv3-plus-pytorch-main\VOCdevkit\VOC2007\SegmentationClass'
-fileDir = 'E:\dataset\\train2014'
+
+def test_dataset(name,fileDir):
+    # img_save_source = 'D:\Program data\pythonProject\deeplabv3-plus-pytorch-main\VOCdevkit\VOC2007\JPEGImages'
+    # seg_save_source = 'D:\Program data\pythonProject\deeplabv3-plus-pytorch-main\VOCdevkit\VOC2007\SegmentationClass'
 
 
-def test_dataset():
     def get_params(img_, scale_):
         b, c, w, h = img_.shape
         print(img_.shape)
@@ -27,10 +27,10 @@ def test_dataset():
             point[1] += i
         return [[0, 0], [w - 1, 0], [w - 1, h - 1], [0, h - 1]], startpoint
 
-    def compose(front_name, back_name, count):
-        count = str(count)
-        seg_save_path = os.path.join(seg_save_source, count + '.png')
-        img_save_path = os.path.join(img_save_source, count + '.jpg')
+    def compose(front_name, back_name):
+        temp = random.randint(1,2)
+        # seg_save_path = os.path.join(seg_save_source, count + '.png')
+        # img_save_path = os.path.join(img_save_source, count + '.jpg')
         img_front_path = os.path.join(fileDir, front_name)
         img_back_path = os.path.join(fileDir, back_name)
         img_back_size = Image.open(img_back_path).size
@@ -44,8 +44,7 @@ def test_dataset():
         img_front = trans(Image.open(img_front_path).convert("RGB")).unsqueeze(0)  # 前景图
         front_mask_0 = torch.ones(img_front.shape)
 
-        i = random.randint(0, 1)  # 0 ->打印纸 1 -> 显示屏
-        if i == 0:
+        if temp % 2 == 0:
             startpoints, endpoints = get_params(img_front, {
                 "area": [0.9, 0.9],
                 "ratio": [1, 1],
@@ -71,10 +70,9 @@ def test_dataset():
             front_mask_0 = F.perspective(front_mask_0, startpoints, endpoints)
 
             img = img_back * (1 - front_mask_0) + img_front  # 带边框的前景嵌入背景图
-            front_mask[0, 2:, ...] *= 0
+            front_mask[0, 1:, ...] *= 0
 
-            torchvision.utils.save_image(front_mask, seg_save_path)
-            torchvision.utils.save_image(img, img_save_path)
+            return front_mask, img
         else:
             startpoints, endpoints = get_params(img_front, {
                 "area": [0.3, 0.4],
@@ -83,7 +81,7 @@ def test_dataset():
             })
             img_front = F.perspective(img_front, startpoints, endpoints)
             front_mask = F.perspective(front_mask_0, startpoints, endpoints)
-            img_front = torch.ones(img_front.shape) * (1 - front_mask) * (np.random.randint(1, 50) / 255) + img_front
+            img_front = torch.ones(img_front.shape) * (1 - front_mask) * (np.random.randint(205, 255) / 255) + img_front
 
             front_mask = torch.ones(img_front.shape)
 
@@ -100,22 +98,23 @@ def test_dataset():
             front_mask_0 = F.perspective(front_mask_0, startpoints, endpoints)
 
             img = img_back * (1 - front_mask_0) + img_front  # 带边框的前景嵌入背景图
-            front_mask[0, 1:, ...] *= 0
+            front_mask[0, 2:, ...] *= 0
 
-            return front_mask,img
+            return front_mask, img
 
-            
-
-    front_key = 200
-    back_key = 200
+    # front_key = 1
+    back_key = 1
     pathDir = os.listdir(fileDir)  # 取图片的原始路径
-    front_img = random.sample(pathDir, front_key)  # 随机选取前景图片
-    back_img = random.sample(pathDir, back_key)  # 随机获取背景图片
-    for count in range(0, len(front_img)):
-        front_name = front_img[count]
-        back_name = back_img[count]
-        compose(front_name, back_name, count)
-
-
-if __name__ == "__main__":
-    test_dataset()
+    # front_img_list = random.sample(pathDir, front_key)  # 随机选取前景图片
+    back_img_list = random.sample(pathDir, back_key)  # 随机获取背景图片
+    # front_name = front_img_list[0]
+    back_name = back_img_list[0]
+    front_mask, img = compose(name, back_name)
+    front_mask = torch.squeeze(front_mask, 0)
+    img = torch.squeeze(img,0)
+    front_mask = torchvision.transforms.functional.to_pil_image(front_mask)
+    img = torchvision.transforms.functional.to_pil_image(img)
+    return front_mask, img
+#
+# if __name__ == "__main__":
+#     test_dataset()
--- detection/train.py
+++ detection/train.py
@@ -12,23 +12,40 @@ from utils.callbacks import LossHistory
 from utils.dataloader import DeeplabDataset, deeplab_dataset_collate
 from utils.utils_fit import fit_one_epoch
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    # -------------------------------#
+    #   是否使用Cuda
+    #   没有GPU可以设置成False
+    # -------------------------------#
     Cuda = True
     # -------------------------------#
-    #
+    #   训练自己的数据集必须要修改的
     #   自己需要的分类个数+1，如2+1
     # -------------------------------#
-    num_classes = 2
+    num_classes = 3
     # -------------------------------------------------------------------#
     #   所使用的的主干网络：
     #   mobilenet、xception
+    #
+    #   在使用xception作为主干网络时，建议在训练参数设置部分调小学习率，如：
+    #   Freeze_lr   = 3e-4
+    #   Unfreeze_lr = 1e-5
     # -------------------------------------------------------------------#
     backbone = "mobilenet"
+    # ----------------------------------------------------------------------------------------------------------------------------#
+    #   是否使用主干网络的预训练权重，此处使用的是主干的权重，因此是在模型构建的时候进行加载的。
+    #   如果设置了model_path，则主干的权值无需加载，pretrained的值无意义。
+    #   如果不设置model_path，pretrained = True，此时仅加载主干开始训练。
+    #   如果不设置model_path，pretrained = False，Freeze_Train = Fasle，此时从0开始训练，且没有冻结主干的过程。
+    # --------------------------------------------------------------------------------------------------------------------------
     pretrained = False
+    # ----------------------------------------------------------------------------------------------------------------------------#
+
+    # ----------------------------------------------------------------------------------------------------------------------------#
     model_path = "model_data/deeplab_mobilenetv2.pth"
     # ---------------------------------------------------------#
-    #   下采样的倍数8、16 
+    #   下采样的倍数8、16
     #   8下采样的倍数较小、理论上效果更好，但也要求更大的显存
     # ---------------------------------------------------------#
     downsample_factor = 16
@@ -36,10 +53,26 @@ if __name__ == "__main__":
     #   输入图片的大小
     # ------------------------------#
     input_shape = [512, 512]
+
+    # ----------------------------------------------------#
+    #   训练分为两个阶段，分别是冻结阶段和解冻阶段。
+    #   显存不足与数据集大小无关，提示显存不足请调小batch_size。
+    #   受到BatchNorm层影响，batch_size最小为2，不能为1。
+    # ----------------------------------------------------#
+    # ----------------------------------------------------#
+    #   冻结阶段训练参数
+    #   此时模型的主干被冻结了，特征提取网络不发生改变
+    #   占用的显存较小，仅对网络进行微调
+    # ----------------------------------------------------#
     Init_Epoch = 0
     Freeze_Epoch = 50
     Freeze_batch_size = 8
     Freeze_lr = 5e-4
+    # ----------------------------------------------------#
+    #   解冻阶段训练参数
+    #   此时模型的主干不被冻结了，特征提取网络会发生改变
+    #   占用的显存较大，网络所有的参数都会发生改变
+    # ----------------------------------------------------#
     UnFreeze_Epoch = 100
     Unfreeze_batch_size = 4
     Unfreeze_lr = 5e-5
@@ -58,6 +91,13 @@ if __name__ == "__main__":
     #   是否使用focal loss来防止正负样本不平衡
     # ---------------------------------------------------------------------#
     focal_loss = False
+    # ---------------------------------------------------------------------#
+    #   是否给不同种类赋予不同的损失权值，默认是平衡的。
+    #   设置的话，注意设置成numpy形式的，长度和num_classes一样。
+    #   如：
+    #   num_classes = 3
+    #   cls_weights = np.array([1, 2, 3], np.float32)
+    # ---------------------------------------------------------------------#
     cls_weights = np.ones([num_classes], np.float32)
     # ------------------------------------------------------#
     #   是否进行冻结训练，默认先冻结主干训练后解冻训练。
@@ -66,6 +106,7 @@ if __name__ == "__main__":
     # ------------------------------------------------------#
     #   用于设置是否使用多线程读取数据
     #   开启后会加快数据读取速度，但是会占用更多内存
+    #   内存较小的电脑可以设置为2或者0
     # ------------------------------------------------------#
     num_workers = 0
 
@@ -74,6 +115,9 @@ if __name__ == "__main__":
     if not pretrained:
         weights_init(model)
     if model_path != '':
+        # ------------------------------------------------------#
+        #   权值文件请看README，百度网盘下载
+        # ------------------------------------------------------#
         print('Load weights {}.'.format(model_path))
         device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         model_dict = model.state_dict()
@@ -93,12 +137,17 @@ if __name__ == "__main__":
     # ---------------------------#
     #   读取数据集对应的txt
     # ---------------------------#
-    with open(os.path.join(VOCdevkit_path, "VOC2007/ImageSets/Segmentation/train.txt"), "r") as f:
-        train_lines = f.readlines()
+    # with open(os.path.join(VOCdevkit_path, "VOC2007/ImageSets/Segmentation/train.txt"), "r") as f:
+    #     train_lines = f.readlines()
+    #
+    # with open(os.path.join(VOCdevkit_path, "VOC2007/ImageSets/Segmentation/val.txt"), "r") as f:
+    #     val_lines = f.readlines()
 
-    with open(os.path.join(VOCdevkit_path, "VOC2007/ImageSets/Segmentation/val.txt"), "r") as f:
-        val_lines = f.readlines()
-
+    supported = [".jpg", ".JPG", ".png", ".PNG", ".bmp", ".BMP"]  # 支持的文件后缀类型
+    train_fileDir = 'E:\dataset\\train2014'
+    val_fileDir = 'E:\dataset\\train2014'
+    train_lines = [i for i in os.listdir(train_fileDir) if os.path.splitext(i)[-1] in supported]
+    val_lines = [i for i in os.listdir(val_fileDir) if os.path.splitext(i)[-1] in supported]
     # ------------------------------------------------------#
     #   主干特征提取网络特征通用，冻结训练可以加快训练速度
     #   也可以在训练初期防止权值被破坏。
@@ -122,8 +171,8 @@ if __name__ == "__main__":
         optimizer = optim.Adam(model_train.parameters(), lr, weight_decay=5e-4)
         lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.94)
 
-        train_dataset = DeeplabDataset(train_lines, input_shape, num_classes, True, VOCdevkit_path)
-        val_dataset = DeeplabDataset(val_lines, input_shape, num_classes, False, VOCdevkit_path)
+        train_dataset = DeeplabDataset(train_lines, input_shape, num_classes, True, train_fileDir)
+        val_dataset = DeeplabDataset(val_lines, input_shape, num_classes, False, val_fileDir)
         gen = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers, pin_memory=True,
                          drop_last=True, collate_fn=deeplab_dataset_collate)
         gen_val = DataLoader(val_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers, pin_memory=True,
@@ -157,8 +206,8 @@ if __name__ == "__main__":
         optimizer = optim.Adam(model_train.parameters(), lr, weight_decay=5e-4)
         lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.94)
 
-        train_dataset = DeeplabDataset(train_lines, input_shape, num_classes, True, VOCdevkit_path)
-        val_dataset = DeeplabDataset(val_lines, input_shape, num_classes, False, VOCdevkit_path)
+        train_dataset = DeeplabDataset(train_lines, input_shape, num_classes, True, train_fileDir)
+        val_dataset = DeeplabDataset(val_lines, input_shape, num_classes, False, val_fileDir)
         gen = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers, pin_memory=True,
                          drop_last=True, collate_fn=deeplab_dataset_collate)
         gen_val = DataLoader(val_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers, pin_memory=True,
